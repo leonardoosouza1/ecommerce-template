b0e7e44757ec09acb87ca8dc10f7f679043156a7
Knyon1 <knyonn1@gmail.com>
2021-08-12 18:33:12 -0300

    dale

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,45 +1,15 @@
-const parse = require('pg-connection-string').parse;
-
-
-module.exports = ({ env }) => {
-
-  if(env('NODE_ENV') == 'production') {
-    const config = parse(process.env.DATABASE_URL)
-    return {
-      defaultConnection: 'default',
-      connections: {
-        default: {
-          connector: 'bookshelf',
-          settings: {
-            client: 'postgres',
-            host: config.host,
-            port: config.port,
-            database: config.database,
-            username: config.username,
-            password: config.password,
-          },
-          options: {
-            ssl: false,
-          },
-        },
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'sqlite',
+        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
       },
-    }
-  }
-  return {
-    defaultConnection: 'default',
-    connections: {
-      default: {
-        connector: 'bookshelf',
-        settings: {
-          client: 'sqlite',
-          filename: env('DATABASE_FILENAME', '.tmp/data.db'),
-        },
-        options: {
-          useNullAsDefault: true,
-        },
+      options: {
+        useNullAsDefault: true,
       },
     },
-  }
-
-
-};
+  },
+});
