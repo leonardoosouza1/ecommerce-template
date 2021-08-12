499e6106ba1a24b470066634ade3835461da6a1b
Knyon1 <knyonn1@gmail.com>
2021-08-12 18:14:41 -0300

    dale

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env('HOST', process.env.HOST),
+  port: env.int('PORT', process.env.PORT),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '03393ef55c3e50cb086a18ab65e24a50'),
